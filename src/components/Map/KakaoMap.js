32399e03ff8f22c51d711e522900a28ac729cd11
kjm541006 <kjm541113@gmail.com>
2022-10-07 01:49:56 +0900

    현재 위치 자동으로 불러오기 가능 및 버튼으로 불러오기 구현

--- src/components/Map/KakaoMap.js
+++ src/components/Map/KakaoMap.js
@@ -1,50 +1,83 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import classes from "./KakaoMap.module.css";
 
 const KakaoMap = () => {
   const kakaoMapRef = useRef(null);
+  const [loading, setLoading] = useState(false);
 
   const getCurrentLocation = () => {
     let gpsOptions = {
       enableHighAccuracy: true,
-      timeout: 7000,
     };
     return new Promise((resolve, rejected) => {
       navigator.geolocation.getCurrentPosition(resolve, rejected, gpsOptions);
     });
   };
 
+  const onLoadCurrentPosition = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const position = await getCurrentLocation();
+      let lat = position.coords.latitude;
+      let log = position.coords.longitude;
+      console.log(position);
+      console.log(lat, log);
+
+      let centerPoint = new window.kakao.maps.LatLng(lat, log);
+      const options = {
+        center: centerPoint,
+        level: 4,
+      };
+      new window.kakao.maps.Map(kakaoMapRef.current, options);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // useEffect(() => {
+  //   if (!kakaoMapRef.current) {
+  //     return;
+  //   }
+  //   const options = {
+  //     center: new window.kakao.maps.LatLng(37.5666805, 126.9784147),
+  //     level: 4,
+  //   };
+  //   new window.kakao.maps.Map(kakaoMapRef.current, options);
+  // }, []);
+
   useEffect(() => {
     if (!kakaoMapRef.current) {
       return;
     }
-
     const drawMap = async () => {
+      setLoading(true);
       try {
         let position = await getCurrentLocation();
         let lat = position.coords.latitude;
         let log = position.coords.longitude;
-
         console.log(lat, log);
-
         let centerPoint = new window.kakao.maps.LatLng(lat, log);
         const options = {
           center: centerPoint,
           level: 4,
         };
         new window.kakao.maps.Map(kakaoMapRef.current, options);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
-
     drawMap();
   }, []);
 
   return (
-    <div>
+    <>
       <div ref={kakaoMapRef} className={classes.kakaomap} />
-    </div>
+      <button onClick={onLoadCurrentPosition}>현재 위치로 이동</button>
+      {loading && <span>현 위치 불러오는 중...</span>}
+    </>
   );
 };
 
